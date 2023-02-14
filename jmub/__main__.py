67d0c880b9b3533d1b35fec9dbf30530c5739ca4
ᵇⁱˢʰⁱ <125081048+bbhh2@users.noreply.github.com>
2023-02-14 16:57:23 +0300

    Update __main__.py

--- jmub/__main__.py
+++ jmub/__main__.py
@@ -18,7 +18,7 @@ from .utils import (
     verifyLoggerGroup,
 )
 
-LOGS = logging.getLogger("سورس جمثون")
+LOGS = logging.getLogger("سورس أكثون")
 
 cmdhr = Config.COMMAND_HAND_LER
 
@@ -58,7 +58,7 @@ async def startup_process():
             LOGS.info("تم انتهاء عملية التنصيب بنجاح")
             LOGS.info(
                 f"لمعرفة اوامر السورس ارسل {cmdhr}الاوامر\
-                \nمجموعة قناة السورس  https://t.me/jmthon_support"
+                \nمجموعة قناة السورس  https://t.me/AkTSupport"
             )
             LOGS.info("============================================================")
             await verifyLoggerGroup()
@@ -71,9 +71,9 @@ async def startup_process():
             LOGS.info(str(e))
             return
     else:
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
-        LOGS.info("انت لا يمكنك تنصيب سورس جمثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس أكثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس أكثون عزيزي دي")
+        LOGS.info("انت لا يمكنك تنصيب سورس أكثون عزيزي دي")
 
 
 jmub.loop.run_until_complete(startup_process())
